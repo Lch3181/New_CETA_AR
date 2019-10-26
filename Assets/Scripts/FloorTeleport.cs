e123fb2ef551e64a790ba64ee982495499bb3fa4
Mitchell7583 <lamchniho@gmail.com>
2019-10-26 16:21:26 -0400

    Added collider for terrains, and teleport buttons to each floors
    +colliders for terrains
    bug: might suck inside some walls due to poor modeling
    clean up lighting maps
    +spawnSpots for teleporting
    +buttons to teleport to each floor

---
+++ Assets/Scripts/FloorTeleport.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class FloorTeleport : MonoBehaviour
+{
+    CharacterController controller;
+    public GameObject buttons;
+
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        if(Cursor.lockState == CursorLockMode.Locked)
+        {
+            buttons.SetActive(false);
+        }
+        else
+        {
+            buttons.SetActive(true);
+        }
+    }
+
+    public void Teleport(GameObject target)
+    {
+        controller.enabled = false;
+        gameObject.transform.position = target.transform.position;
+        controller.enabled = true;
+    }
+}
