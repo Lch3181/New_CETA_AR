581e4fa3caf617a032b14d834bca8d4e91225e75
Chris Nguyen <christopher.nguyen@snhu.edu>
2019-12-16 20:13:38 -0500

    Update
    -Added Layer Update Triggers
    -Fixed 2nd Floor not appearing in the 1st floor lobby.
    -Seperated Layer Display from Teleport Script.

---
+++ Assets/Scripts/CETA Scripts/FloorLayerDisplay.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloorLayerDisplay : MonoBehaviour
+{
+    public GameObject[] Floors;
+    public GameObject[] Triggers;
+
+    public void SetCurrentLayer(GameObject go)
+    {
+        ResetLayer();
+        go.layer = LayerMask.NameToLayer("CurrentFloor");
+        for (int i = System.Array.IndexOf(Floors, go); i >= 0; i--)
+        {
+            foreach (Transform child in Triggers[i].transform)
+            {
+                child.gameObject.layer = LayerMask.NameToLayer("CurrentFloor");
+            }
+            foreach (Transform child in Floors[i].transform)
+            {
+                child.gameObject.layer = LayerMask.NameToLayer("CurrentFloor");
+            }
+        }
+    }
+
+    public void ResetLayer()
+    {
+        foreach (GameObject floor in Floors)
+        {
+            floor.layer = 10;
+            foreach (Transform child in floor.transform)
+            {
+                child.gameObject.layer = 10;
+            }
+        }
+
+        foreach (GameObject trigger in Triggers)
+        {
+            trigger.layer = 10;
+            foreach (Transform child in trigger.transform)
+            {
+                child.gameObject.layer = 10;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/CETA Scripts/FloorLayerTrigger.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloorLayerTrigger : MonoBehaviour
+{
+    public GameObject floorLayer;
+    private FloorLayerDisplay layerDisplay;
+
+    private void Start()
+    {
+        layerDisplay = GameObject.Find("Player").GetComponent<FloorLayerDisplay>();
+    }
+
+
+    private void OnTriggerEnter(Collider hit)
+    {
+        if(hit.tag == "Player")
+        {
+            Debug.Log("Setting Layer.");
+            layerDisplay.SetCurrentLayer(floorLayer);
+        }
+    }
+}
